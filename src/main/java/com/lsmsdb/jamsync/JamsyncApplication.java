55e248d8b2de4c576f38fa88f2963813607b210b
Daniel Namaki <namaki.work@gmail.com>
2023-12-14 14:40:07 +0100

    springboot initial upload

---
+++ src/main/java/com/lsmsdb/jamsync/JamsyncApplication.java
@@ -0,0 +1,13 @@
+package com.lsmsdb.jamsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsmsdb/jamsync/JamsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsmsdb.jamsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
