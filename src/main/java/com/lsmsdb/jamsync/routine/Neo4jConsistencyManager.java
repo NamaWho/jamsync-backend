74a9bd063651fbcb1b668a5404720ce10f611598
Daniel Namaki <namaki.work@gmail.com>
2024-01-22 17:08:20 +0100

    add Neo4jConsistencyManager routine

--- src/main/java/com/lsmsdb/jamsync/routine/Neo4jConsistencyManager.java
+++ src/main/java/com/lsmsdb/jamsync/routine/Neo4jConsistencyManager.java
@@ -2,6 +2,7 @@ package com.lsmsdb.jamsync.routine;
 
 // Neo4jConsistencyManager.java
 
+import com.lsmsdb.jamsync.repository.Neo4jDriver;
 import org.neo4j.driver.*;
 
 import java.util.ArrayList;
@@ -28,7 +29,32 @@ public class Neo4jConsistencyManager {
     public void pushOperation(String operation) {
         operations.add(operation);
     }
-    private void retryOperation() {}
+
+    private String popOperation() {
+        if (!operations.isEmpty()) {
+            String operation = operations.get(0);
+            operations.remove(0);
+            return operation;
+        }
+        return null;
+    }
+
+    private void retryOperation() {
+        String operation = popOperation();
+        List<String> failedOperations = new ArrayList<>();
+        while (operation != null) {
+            try {
+                Session session = Neo4jDriver.getInstance().getDriver().session();
+                session.run(operation);
+                session.close();
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                failedOperations.add(operation);
+            }
+            operation = popOperation();
+        }
+        operations = failedOperations;
+    }
 
     public void shutdown() {
         scheduler.shutdown();
